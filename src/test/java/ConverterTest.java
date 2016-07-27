d6184a7496b208ab42ab06a14e84ca4574b3e545
noskovii <noskovii@mail.ru>
2016-07-28 02:08:33 +0700

    add screenshots in tests

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -10,13 +10,14 @@ import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameter;
 import org.junit.runners.Parameterized.Parameters;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import ru.yandex.qatools.allure.annotations.Attachment;
+import ru.yandex.qatools.allure.annotations.Step;
+import ru.yandex.qatools.allure.annotations.Title;
 
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
@@ -33,33 +34,45 @@ public class ConverterTest
         driver.get("http://www.sberbank.ru/ru/person");
     }
 
+    @Attachment(value = "Page screenshot", type = "image/png")
+    protected byte[] saveAllureScreenshot()
+    {
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+    }
+
     @Test
+    @Title("Test case of convert to field")
     public void testConvertTo()
     {
         WebElement from = driver.findElement(By.xpath(".//*[@id='from']"));
         from.clear();
         from.sendKeys(ParametersParser.getInstance().getTestConvertToParam());
 
+        saveAllureScreenshot();
         assertThat(driver.findElement(By.xpath(".//*[@id='to']")).getAttribute("value"), is(not("")));
     }
 
     @Test
+    @Title("Test case of correct input in from field")
     public void testCorrectNumFrom()
     {
         WebElement from = driver.findElement(By.xpath(".//*[@id='from']"));
         from.clear();
         from.sendKeys(ParametersParser.getInstance().getTestCorrectNumFromParam());
 
+        saveAllureScreenshot();
         assertThat(driver.findElement(By.xpath(".//*[@id='from']")).getText(), is(""));
     }
 
     @Test
+    @Title("Test case of correct input in to field")
     public void testCorrectNumTo()
     {
         WebElement from = driver.findElement(By.xpath(".//*[@id='to']"));
         from.clear();
         from.sendKeys(ParametersParser.getInstance().getTestCorrectNumToParam());
 
+        saveAllureScreenshot();
         assertThat(driver.findElement(By.xpath(".//*[@id='to']")).getText(), is(""));
     }
 
